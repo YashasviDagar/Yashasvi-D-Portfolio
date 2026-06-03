bb46c66464f069af3583cb54a6762a7a74419eaa
Yashasvi Dagar <dagaryashu2046@gmail.com>
2026-06-03 23:09:50 +0530

    Installation and adding files

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import About from './components/About/About'
+import Skills from './components/Skills/Skills'
+import Experience from './components/Experience/Experience'
+import Work from './components/Work/Work'
+import Education from './components/Education/Education'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      
-    </>
+    <div className='bg-[#050414]'>
+      <div className='absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px] [mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'>
+        {/* this className makes the grid like structure in the background of the site. */}
+      </div>
+
+
+      <div className='relative pt-20'>
+        <Navbar />
+        <About />
+        <Skills />
+        <Experience />
+        <Work />
+        <Education />
+        <Contact />
+        <Footer />
+      </div>
+    </div>
   )
 }
 
