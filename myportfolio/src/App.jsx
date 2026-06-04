6002a94aadc9bcead743c2bb917bcb30ad5ae12d
Yashasvi Dagar <dagaryashu2046@gmail.com>
2026-06-04 12:26:38 +0530

    Added BlurBlob effect

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -8,12 +8,16 @@ import Education from './components/Education/Education'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import './App.css'
+import BlurBlob from './components/BlurBlob'
 
 function App() {
 
 
   return (
     <div className='bg-[#050414]'>
+
+      <BlurBlob position={{top: '35%', left: '20%'}} size={{width: '30%', height: '40%'}}></BlurBlob>
+
       <div className='absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px] [mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]'>
         {/* this className makes the grid like structure in the background of the site. */}
       </div>
